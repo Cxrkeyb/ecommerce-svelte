9c92eb04c94f90ceceba3ad5e4cc02b4577122d7
Cxrkeyb <cxrkeybwp2004@gmail.com>
2024-06-13 04:52:52 -0500

    <Feat> Add functionality to "/products/[id]" for add to the cart

--- src/routes/products/[id]/+page.svelte
+++ src/routes/products/[id]/+page.svelte
@@ -1,32 +1,20 @@
-<!-- src/routes/item/[id].svelte -->
 <script lang="ts">
-	// Importa el contexto del entorno de SvelteKit
 	import { page } from '$app/stores';
-	import { onMount } from 'svelte';
-  import Products from '$lib/components/Products.svelte';
+	import { onMount, afterUpdate, onDestroy } from 'svelte';
+	import { addItem, cart } from '$lib/context/cartContext';
+	import { goto } from '$app/navigation';
+	import type { Product } from '$lib/types/Product.d.ts';
+	import Products from '$lib/components/Products.svelte';
 
 	// Obtiene el id de la URL actual
 	export let id = $page.params.id;
 
-	// Puedes usar `id` en tu lógica para cargar datos, etc.
-
-	interface Product {
-		id: number;
-		name: string;
-		description: string;
-		price: number;
-		image: string;
-		stock: number;
-	}
-
 	let quantity = 1;
-
-	// Get the product from the server
 	let product: Product | null = null;
 	let error: string | null = null;
 
-	// Load the product from the server
-	const loadProduct = async () => {
+	const loadProduct = async (id: string = $page.params.id) => {
+		console.log(`Loading product with id: ${id}`);
 		try {
 			const response = await fetch(`http://localhost:3100/v1/products/${id}`, {
 				method: 'GET',
@@ -36,6 +24,8 @@
 			});
 			if (response.ok) {
 				product = await response.json();
+				console.log('Product loaded:', product);
+				quantity = 1;
 			} else {
 				error = `Error fetching product: ${response.status} ${response.statusText}`;
 				console.error(error);
@@ -46,26 +36,66 @@
 		}
 	};
 
-	onMount(loadProduct);
+	// Cargar el producto al montar el componente
+	onMount(() => {
+		loadProduct();
+	});
 
+	// Función para añadir al carrito
 	const addToCart = () => {
-		console.log('Add to cart');
+		if (!product) return;
+
+		const item = $cart.find((item) => item.id === product!.id);
+
+		if (item) {
+			goto('/cart');
+			return;
+		}
+
+		addItem({
+			id: product.id,
+			name: product.name,
+			price: product.price,
+			quantity: quantity,
+			image: product.image
+		});
 	};
 
+	// Función para incrementar la cantidad
 	const increaseQuantity = () => {
-		if (product && product.stock > quantity) {
+		if (product && product.stock && product.stock > quantity) {
 			quantity += 1;
 		}
 	};
 
+	// Función para decrementar la cantidad
 	const decreaseQuantity = () => {
 		if (quantity > 1) {
 			quantity -= 1;
 		}
 	};
+
+	// Suscribirse al estado del carrito para actualizar
+	const unsubscribeCart = cart.subscribe((value) => {
+		console.log('Cart updated:', value);
+	});
+
+	// Suscribirse a los cambios de página para detectar cambios en los parámetros de la URL
+	const unsubscribeProduct = page.subscribe(($page) => {
+		const productId = $page.params.id;
+		if (productId) {
+			loadProduct(productId);
+		}
+	});
+
+	// Limpiar la suscripción al desmontar el componente
+	onDestroy(() => {
+		unsubscribeCart();
+		unsubscribeProduct();
+	});
 </script>
 
-<div class="flex flex-col items-center justify-center py-20 ">
+<div class="flex flex-col items-center justify-center py-20">
 	<div class="border shadow-xl bg-white container rounded-xl">
 		{#if product}
 			<div class="flex flex-row items-center justify-center p-2">
@@ -75,15 +105,16 @@
 						{product.name}
 					</h2>
 					<p class="text-lg text-gray-600 mb-4">
-						{product.description}
+						{product.description && product.description}
 					</p>
 					<p class="text-xl font-bold text-primary">
 						${product.price}
 					</p>
-          <p class="text-lg text-gray-600">
-            Stock: {product.stock}
-          </p>
-					<!-- Quantity -->
+					<p class="text-lg text-gray-600">
+						Stock: {product.stock}
+					</p>
+
+					<!-- Cantidad -->
 					<div class="flex flex-row items-center justify-center mt-4">
 						<button
 							class="bg-primary text-white px-4 py-2 rounded-lg disabled:opacity-50 {quantity === 1
@@ -106,10 +137,14 @@
 							+
 						</button>
 					</div>
-
-					<!-- Boton add to cart -->
+					<!-- Botón agregar al carrito -->
 					<button class="bg-primary text-white px-4 py-2 rounded-lg mt-4" on:click={addToCart}>
-						Add to Cart
+						<!-- Si el producto ya está en el carrito cambiar el texto -->
+						{#if $cart.find((item) => product && item.id === product.id)}
+							Go to cart
+						{:else}
+							Add to Cart
+						{/if}
 					</button>
 				</div>
 			</div>
@@ -120,4 +155,4 @@
 		{/if}
 	</div>
 </div>
-<Products limit={4}  />
+<Products limit={4} />
