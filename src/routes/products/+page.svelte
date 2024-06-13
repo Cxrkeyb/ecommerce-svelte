56106b98a8853e25c7b1fabf88738035b9eff0a9
Cxrkeyb <cxrkeybwp2004@gmail.com>
2024-06-12 19:55:15 -0500

    <Feat> Create page for show the products

---
+++ src/routes/products/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+  import LargeCard from '$lib/components/cards/LargeCard.svelte';
+  import DogsPossing from '$lib/assets/dogs-possing.png';
+  import Products from '$lib/components/Products.svelte';
+</script>
+
+
+<LargeCard
+	imgurl={DogsPossing}
+	bgColor="bg-secondary-light"
+  textColor="text-white"
+	title="One More Friend"
+	subtitle="Thousands More Fun!"
+	description="Having a pet means you have more joy, a new friend, a happy person who will always be with you to have fun. We have 200+ different pets that can meet your needs!"
+  alignment="right"
+/>
+<h2 class="text-3xl font-bold text-center mt-8 text-primary">Our Products</h2>
+<Products header={false} button={false} pagination={true} />
